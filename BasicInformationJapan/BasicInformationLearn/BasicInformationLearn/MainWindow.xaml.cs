aa4958d331327174ddf84df81134e5d2039c8bb2
sakuriver <suzuerain@yahoo.co.jp>
2021-09-25 19:16:18 +0900

    令和元年_午前問題_問３ビューワーを作成中

---
+++ BasicInformationJapan/BasicInformationLearn/BasicInformationLearn/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicInformationLearn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<int> drawLines;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SettingTextArea.Visibility = Visibility.Hidden;
+        }
+        private void SettingTextArea_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void SettingViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SettingTextArea.Visibility == Visibility.Hidden)
+            {
+                SettingTextArea.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                SettingTextArea.Visibility = Visibility.Hidden;
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CalcStartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
