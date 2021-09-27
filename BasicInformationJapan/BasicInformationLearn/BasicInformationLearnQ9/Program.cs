cd2251bc88b5bafc5361558a5c407e2e54ba8f51
sakuriver <suzuerain@yahoo.co.jp>
2021-09-28 01:14:50 +0900

    基本情報技術者試験_令和元年の午前問題をプログラム化した
    迷路の回転プログラム

---
+++ BasicInformationJapan/BasicInformationLearn/BasicInformationLearnQ9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+namespace BasicInformationLearnQ9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> dataList = new List<List<int>>();
+            // 初期の配列状態を表す
+            dataList.Add(new List<int>() { 0, 1, 1, 1, 1, 1, 1, 0 });
+            dataList.Add(new List<int>() { 0, 1, 0, 0, 0, 0, 0, 0 });
+            dataList.Add(new List<int>() { 0, 1, 0, 0, 0, 0, 0, 0 });
+            dataList.Add(new List<int>() { 0, 1, 1, 1, 1, 0, 0, 0 });
+            dataList.Add(new List<int>() { 0, 1, 0, 0, 0, 0, 0, 0 });
+            dataList.Add(new List<int>() { 0, 1, 0, 0, 0, 0, 0, 0 });
+            dataList.Add(new List<int>() { 0, 1, 0, 0, 0, 0, 0, 0 });
+            dataList.Add(new List<int>() { 0, 1, 0, 0, 0, 0, 0, 0 });
+
+            // 初期状態の描画をする
+            foreach (var dataRow in dataList) {
+                foreach (var dataCeil in dataRow) {
+                    Console.Write("|" + dataCeil.ToString() + "|");
+                }
+                Console.Write("\r\n");
+                // ちょっとわかりやすくアニメーション
+                Thread.Sleep(1000);
+            }
+            // 方向切り替えアクション
+            Thread.Sleep(2000);
+            Console.Write("\r\n");
+            Console.Write("\r\n");
+
+            List<List<int>> dataAfterList = new List<List<int>>();
+
+            for (var i = 0; i < dataList.Count; i++)
+            {
+               dataAfterList.Add(new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0 });
+            }
+
+            for (var i = 0; i < dataList.Count; i++) {
+                for (var j = 0; j < dataList[i].Count; j++) {
+                    dataAfterList[j][dataList.Count - (i + 1)] = dataList[i][j];
+                }
+            }
+
+            for (var i = 0; i < dataList.Count; i++)
+            {
+                for (var j = 0; j < dataList[i].Count; j++)
+                {
+                    Console.Write("|" + dataAfterList[i][j] + "|");
+                }
+                Console.Write("\r\n");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
