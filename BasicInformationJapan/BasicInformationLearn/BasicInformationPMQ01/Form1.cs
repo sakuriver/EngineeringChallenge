4d432c48ca3ac679cf4ebf777583b5426d5daad5
sakuriver <suzuerain@yahoo.co.jp>
2021-09-30 00:36:54 +0900

    基本情報技術者試験
         ・午後問題のネットワーク編のレイアウト用意と分岐部分を用意
    　・午前問題の用語調査

---
+++ BasicInformationJapan/BasicInformationLearn/BasicInformationPMQ01/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicInformationPMQ01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WorkManager work = new WorkManager();
+            work.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NetWorkManager netWorkManager = new NetWorkManager();
+            netWorkManager.Show();
+        }
+    }
+}
---
+++ BasicInformationJapan/BasicInformationLearn/BasicInformationPMQ01/NetworkManager.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace BasicInformationPMQ01
+{
+    partial class NetWorkManager
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ルール番号 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.送信元 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.宛先 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.サービス = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.動作 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MachineName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(102, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(504, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ログイン情報を入力してください";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(44, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 54);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(44, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 54);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 57);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ログイン";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(749, 414);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(741, 386);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "監査君入室";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(741, 386);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "フィルタリングシミュレータ";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ルール番号,
+            this.送信元,
+            this.宛先,
+            this.サービス,
+            this.動作});
+            this.dataGridView1.Location = new System.Drawing.Point(26, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(658, 330);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ルール番号
+            // 
+            this.ルール番号.HeaderText = "ルール番号";
+            this.ルール番号.Name = "ルール番号";
+            // 
+            // 送信元
+            // 
+            this.送信元.HeaderText = "送信元";
+            this.送信元.Name = "送信元";
+            // 
+            // 宛先
+            // 
+            this.宛先.HeaderText = "宛先";
+            this.宛先.Name = "宛先";
+            // 
+            // サービス
+            // 
+            this.サービス.HeaderText = "サービス";
+            this.サービス.Name = "サービス";
+            // 
+            // 動作
+            // 
+            this.動作.HeaderText = "動作";
+            this.動作.Name = "動作";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(741, 386);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "社内ネット住所確認くん";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 24);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(741, 386);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "PC配置計算君";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.label5);
+            this.tabPage5.Controls.Add(this.label4);
+            this.tabPage5.Controls.Add(this.dataGridView3);
+            this.tabPage5.Controls.Add(this.dataGridView2);
+            this.tabPage5.Location = new System.Drawing.Point(4, 24);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(741, 386);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "物理一覧情報君";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.MachineName});
+            this.dataGridView2.Location = new System.Drawing.Point(67, 53);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(568, 132);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // No
+            // 
+            this.No.HeaderText = "番号";
+            this.No.Name = "No";
+            // 
+            // MachineName
+            // 
+            this.MachineName.HeaderText = "機材名";
+            this.MachineName.Name = "MachineName";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(67, 248);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 25;
+            this.dataGridView3.Size = new System.Drawing.Size(240, 119);
+            this.dataGridView3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(67, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 28);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "操作端末一覧";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(67, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 28);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "部屋一覧";
+            // 
+            // NetWorkManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "NetWorkManager";
+            this.Text = "監査報告君";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ルール番号;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 送信元;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 宛先;
+        private System.Windows.Forms.DataGridViewTextBoxColumn サービス;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 動作;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MachineName;
+    }
+}
+
