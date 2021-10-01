fa9b2f408e8507d1799ac09fb80b061687d9ca8e
sakuriver <suzuerain@yahoo.co.jp>
2021-10-01 10:32:04 +0900

    基本情報技術者試験のモジュール関連準備
    ・午後の問２や問８などのメモリやアルゴリズム系用のライブラリ化用のプロジェクトを用意
    ・問８の関数一つ目から

---
+++ BasicInformationJapan/BasicInformationLearn/BasicInformationLearnLib/StringDataSearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicInformationLearnLib
+{
+    /// <summary>
+    /// 文字列のデータ検索のアルゴリズム関係の対応をするクラス
+    /// 基本情報技術者試験の午後問の問８に該当
+    /// </summary>
+    public class StringDataSearch
+    {
+
+
+        /// <summary>
+        /// 検索対象文字列
+        /// </summary>
+        private List<char> searchText { get; set; }
+
+
+        /// <summary>
+        /// 検索文字列側
+        /// </summary>
+        private List<char> pat;
+
+        public List<char> GenerateBitMask()
+        {
+            return new List<char>();
+        }
+    }
+}
